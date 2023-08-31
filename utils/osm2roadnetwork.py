192c384fd71a08c7a861c17685271f7385b510b2
Gurban <77738468+baygaliyev@users.noreply.github.com>
2023-08-31 12:40:39 +0200

    Update osm2roadnetwork.py

--- utils/osm2roadnetwork.py
+++ utils/osm2roadnetwork.py
@@ -478,8 +478,8 @@ if __name__ == "__main__":
         exit(-1)
 
     # load area range
+    print(rings_ranges)
     ___ring_ranges = [kv[1] for kv in rings_ranges.items() if sys.argv[1].split('/')[-1] == kv[0]]
-    print(___ring_ranges)
     assert len(___ring_ranges) == 1
     ring_ranges = ___ring_ranges[0]
 
