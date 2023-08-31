43c83e376cb2c940a2d4a4aef131493b4d095b96
Gurban <77738468+baygaliyev@users.noreply.github.com>
2023-08-31 12:50:45 +0200

    Update osm2roadnetwork.py

--- utils/osm2roadnetwork.py
+++ utils/osm2roadnetwork.py
@@ -479,7 +479,7 @@ if __name__ == "__main__":
 
     # load area range
     print(rings_ranges)
-    ___ring_ranges = [kv[1] for kv in rings_ranges.items() if sys.argv[1].split('/')[-1] == kv[0]]
+    ___ring_ranges = [kv[1] for kv in rings_ranges.items() if sys.argv[1].split('/')[-1].replace(".xml","") == kv[0]]
     print(sys.argv[1].split('/')[-1])
     assert len(___ring_ranges) == 1
     ring_ranges = ___ring_ranges[0]
