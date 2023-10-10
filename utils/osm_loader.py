77a7360c88709aeb01d1e2c144a1bd39ef3fe8b2
Gurban <77738468+baygaliyev@users.noreply.github.com>
2023-10-10 13:40:02 +0200

    Update osm_loader.py

--- utils/osm_loader.py
+++ utils/osm_loader.py
@@ -160,6 +160,7 @@ class OSMLoader:
         self.edge_index = EdgeIndex(self.adj_segments_graph, self.seg_id_to_idx_in_adj_seg_graph)
 
         logging.info(self.seg_id_to_idx_in_adj_seg_graph)    # debug
+        logging.info(len(self.seg_id_to_idx_in_adj_seg_graph))    # debug
 
         # segment features
         # self.seg_feats has dependency on self.segid_in_adj_segments_graph
