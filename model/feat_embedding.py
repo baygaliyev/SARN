d4dc53c1d694321b6116a49098b4bc5f8500b97d
Gurban <77738468+baygaliyev@users.noreply.github.com>
2024-01-03 15:13:36 +0100

    Update feat_embedding.py

--- model/feat_embedding.py
+++ model/feat_embedding.py
@@ -4,21 +4,23 @@ import torch.nn as nn
 
 from config import Config as Config
 
+# nlanes_code added 
 class FeatEmbedding(nn.Module):
     def __init__(self, nwayid_code, nsegid_code, nhighway_code, 
-            nlength_code, nradian_code, nlon_code, nlat_code):
+            nlength_code, nradian_code, nlon_code, nlat_code, nlanes_code):
             
         super(FeatEmbedding, self).__init__()
 
-        logging.debug('FeatEmbedding args. {}, {}, {}, {}, {}, {}, {}'.format( \
+        logging.debug('FeatEmbedding args. {}, {}, {}, {}, {}, {}, {}, {}'.format( \
                         nwayid_code, nsegid_code, nhighway_code, nlength_code, \
-                        nradian_code, nlon_code, nlat_code))
+                        nradian_code, nlon_code, nlat_code, nlanes_code))
         
         self.emb_highway = nn.Embedding(nhighway_code, Config.sarn_seg_feat_highwaycode_dim) #16
         self.emb_length = nn.Embedding(nlength_code, Config.sarn_seg_feat_lengthcode_dim)    #16
         self.emb_radian = nn.Embedding(nradian_code, Config.sarn_seg_feat_radiancode_dim)    #16
         self.emb_lon = nn.Embedding(nlon_code, Config.sarn_seg_feat_lonlatcode_dim)          #32
         self.emb_lat = nn.Embedding(nlat_code, Config.sarn_seg_feat_lonlatcode_dim)          #32
+        self.emb_lanes = nn.Embedding(nlanes_code, Config.sarn_seg_feat_lencode_dim)   #16, you can also directly write the number
 
     # inputs = [N, nfeat]
     def forward(self, inputs):
@@ -29,4 +31,5 @@ class FeatEmbedding(nn.Module):
                 self.emb_lon(inputs[: , 5]),
                 self.emb_lat(inputs[: , 6]),
                 self.emb_lon(inputs[: , 7]),
-                self.emb_lat(inputs[: , 8])), dim = 1)
+                self.emb_lat(inputs[: , 8])
+                self.emb_lanes(inputs[: , 9])), dim = 1)    # added by me
