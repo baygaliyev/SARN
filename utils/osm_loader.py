6ecceee2b4744e9d476b5886ebde47fcdd24ca6c
Gurban <77738468+baygaliyev@users.noreply.github.com>
2024-02-22 17:32:18 +0100

    Update osm_loader.py
    data type for degree

--- utils/osm_loader.py
+++ utils/osm_loader.py
@@ -99,6 +99,7 @@ class OSMLoader:
         self.segments['c_centrality_code'] = (self.segments.c_centrality * 10000000).astype('int64')    # NEW
         self.segments['b_centrality_code'] = (self.segments.b_centrality * 10000000).astype('int64')    # NEW
         self.segments['h_centrality_code'] = (self.segments.h_centrality * 10000000).astype('int64')    # NEW
+        #self.segments['degree_code'] = (self.segments.degree * 1).astype('int64')    # NEW
 
         # add lat and lon code 
         _lon_unit = 50 * Config.dataset_lon2Euc_unit # 50 meters
@@ -118,6 +119,7 @@ class OSMLoader:
         #self.segments.loc[(self.segments.h-centrality =='NG'), 'lanes'] = 0
         #self.segments.loc[(self.segments.degree =='NG'), 'lanes'] = 0
         self.segments.lanes = self.segments.lanes.astype('int32')
+        self.degree.lanes = self.segments.degree.astype('int32')    # NEW
         #self.segments.lanes
 
         # cellid of segment base on self.cellspace # caution, we have multiple cellspaces, ~= 1000meters
